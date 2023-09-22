00eee3d9dacb6ff92ecb775e15d6263c308e93fa
0qy <fordnicholasjones@gmail.com>
2023-09-22 13:21:34 +1200

    tidy up escape sequences

--- crawl.sh
+++ crawl.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/bash
 
-echo -e "\e[1;34m Welcome to crawl.sh \e[0m"
-echo -e "\e[1;34m Paste url's below and hit enter to clone html directly from the web.\e[0m"
-echo -e "\e[1;34m Press [Ctrl + C] to escape.\e[0m"
+COLOR_BLUE="\x1b[1;34m"
+COLOR_GREEN="\x1b[1;32m"
+COLOR_RED="\x1b[1;31m"
+RESET="\e[0m"
+
+echo -e "${COLOR_BLUE} Welcome to crawl.sh \e[0m"
+echo -e "${COLOR_BLUE} Paste url's below and hit enter to clone html directly from the web.${RESET}"
+echo -e "${COLOR_BLUE} Press [Ctrl + C] to escape.${RESET}"
 
 while :                                                                             #   initialise the program
 do
     read                                                                            #   read user input, $REPLY with result after ENTER keystroke
-    getdate=$(date | sed 's/ //g')                                                  #   get the current date, remove spaces with POSIX expression
+    GET_DATE=$(date | sed 's/ //g')                                                  #   get the current date, remove spaces with POSIX expression
     curl -output $REPLY                                                             #   use curl to connect to the input url, the -output flag indicates to return in html
 
     if [ -f "utput" ]                                                               #   check a file named "utput" exists (default returned filename from previous expression)
     then
         mv utput ./snapshots                                                        #   move the file into snapshots directory
         cd ./snapshots                                                              #   change directory into snapshots
-        mv utput ${getdate}.html                                                    #   rename "utput" to the value of getdate + ".html"
+        mv utput ${GET_DATE}.html                                                    #   rename "utput" to the value of getdate + ".html"
         cd ../                                                                      #   return to the projects base directory
 
-        echo -e "\e[1;32m Snapshot success! \e[0m"
+        echo -e "${COLOR_GREEN} Snapshot success! ${RESET}"
     else
-        echo -e "\e[1;31m Snapshot failed, please try again! \e[0m"
+        echo -e "${COLOR_RED} Snapshot failed, please try again! ${RESET}"
     fi
 done
